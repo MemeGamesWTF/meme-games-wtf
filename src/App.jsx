6dc7f99dae1f3891b91857a40b7d760ea11b656d
MovinduSenura <it22371218@my.sliit.lk>
2024-11-22 13:47:33 +0530

    helmet added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Helmet } from "react-helmet";
 import HomePage from "./components/HomePage";
-import './index.css';
+import "./index.css";
 
 function App() {
-
   return (
-    <>      
-      <BrowserRouter>
+    <>
+      <Helmet>
+        <meta name="description" content="We are MemeGames.WTF" />
+        <meta name="author" content="MemeGames.WTF" />
+        <meta
+          name="keywords"
+          content="Gaming, Meme, Meme Games, Game Development, Gaming Studio"
+        />
+      </Helmet>
+      <BrowserRouter basename="/">
         <Routes>
-          <Route path='/' element={<HomePage />} />
-        </Routes> 
+          <Route path="/" element={<HomePage />} />
+          <Route path="/*" element={<div>notfound</div>} />
+        </Routes>
       </BrowserRouter>
     </>
   );
