2c4fcd26a7ec6ed1ed03d56cc8e058faa44db941
MovinduSenura <it22371218@my.sliit.lk>
2024-12-02 14:32:40 +0530

    json script added

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,251 +1,40 @@
 import React from "react";
 import "./HomePage.css";
 import NavBar from "./NavBar";
-// import Footer from "./Footer";
 import Footer from "./Footer2";
-import play from "../assets/play2.png";
-import tap from "../assets/tap.png";
-import gamelogo1 from "../assets/taptapamerica.webp";
-import gamelogo2 from "../assets/tysonbutt2.webp";
-import gamelogo3 from "../assets/dogegame2.webp";
-import gamelogo4 from "../assets/elongame2.webp";
-import gamelogo5 from "../assets/martian.webp";
-import gamelogo6 from "../assets/mash.webp";
-import gamelogo7 from "../assets/something.webp";
-import gamelogo8 from "../assets/comingsoon3.webp";
-import gamelogo9 from "../assets/comingsoon4.webp";
-// import movingImage from "../assets/thunder.png";
+import gamesData from "./gamesData";
 
 const HomePage = () => {
-  // const numberOfImages = 200;
   return (
     <>
       <div className="main0">
-      {/* <div className="background-animation">
-          <img src={movingImage} alt="Moving Background" className="moving-image" />
-        </div> */}
-        {/* Background animation */}
-        {/* Background animation */}
-        {/* <div className="background-animation">
-          {Array.from({ length: numberOfImages }).map((_, index) => {
-            const randomDuration = 10 + Math.random() * 10; // Random duration between 10s and 20s
-            const randomDelay = Math.random() * 5; // Random delay between 0s and 5s
-            const randomTop = Math.random() * 100; // Random vertical position
-            const randomLeft = Math.random() * 100; // Random horizontal position
-
-            return (
-              <img
-                key={index}
-                src={movingImage}
-                alt="Moving Background"
-                className="moving-image"
-                style={{
-                  animationDuration: `${randomDuration}s`,
-                  animationDelay: `${randomDelay}s`,
-                  top: `${randomTop}vh`,
-                  left: `${randomLeft}vw`,
-                }}
-              />
-            );
-          })}
-        </div> */}
         <NavBar />
         <div className="main1">
           <div className="main2">
-            <a
-              href="http://game.taptapamerica.xyz/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <div className="main3 group">
-                <img
-                  src={gamelogo1}
-                  alt="Description of Image"
-                  loading="lazy"
-                  className="imageclass"
-                />
-                <div className="main4"></div>
-                <span className="spanclass">
-                  <img src={play}/>
-                </span>
-              </div>
-            </a>
-
-            <a
-              // href="https://t.me/meme_games_wtf_bot/tysontaptapbut"
-              href="https://tysonbutt.memegames.wtf/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <div className="main3 group">
-                <img
-                  src={gamelogo2}
-                  alt="Description of Image"
-                  loading="lazy"
-                  className="imageclass"
-                />
-                <div className="main4"></div>
-                <span className="spanclass">
-                  <img src={play}/>
-                </span>
-              </div>
-            </a>
-            <a
-              // href="https://t.me/meme_games_wtf_bot/tysontaptapbut"
-              href="https://elon.memegames.wtf/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <div className="main3 group">
-                <img
-                  src={gamelogo3}
-                  alt="Description of Image"
-                  loading="lazy"
-                  className="imageclass"
-                />
-                <div className="main4"></div>
-                <span className="spanclass">
-                  <img src={play}/>
-                </span>
-              </div>
-            </a>
-            <a
-              // href="https://t.me/meme_games_wtf_bot/tysontaptapbut"
-              href="https://doge.memegames.wtf/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <div className="main3 group">
-                <img
-                  src={gamelogo4}
-                  alt="Description of Image"
-                  loading="lazy"
-                  className="imageclass"
-                />
-                <div className="main4"></div>
-                <span className="spanclass">
-                  <img src={play}/>
-                </span>
-              </div>
-            </a>
-            <a
-              href="https://whackamartian.memegames.wtf/"
-              // href=""
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <div className="main3 group">
-                <img
-                  src={gamelogo5}
-                  alt="Description of Image"
-                  loading="lazy"
-                  className="imageclass"
-                />
-                <div className="main4"></div>
-                <span className="spanclass">
-                  <img src={play}/>
-                </span>
-              </div>
-            </a>
-            {/* <a
-              href="http://mememash.memegames.wtf/"
-              // href=""
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <div className="main3 group">
-                <img
-                  src={gamelogo6}
-                  alt="Description of Image"
-                  loading="lazy"
-                  className="imageclass"
-                />
-                <div className="main4"></div>
-                <span className="spanclass">
-                  <img src={play}/>
-                </span>
-              </div>
-            </a> */}
-            <a
-              href="https://letmetellyousomething.memegames.wtf/"
-              // href=""
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <div className="main3 group">
-                <img
-                  src={gamelogo7}
-                  alt="Description of Image"
-                  loading="lazy"
-                  className="imageclass"
-                />
-                <div className="main4"></div>
-                <span className="spanclass">
-                  <img src={play}/>
-                </span>
-              </div>
-            </a>
-            <a
-              // href="https://t.me/meme_games_wtf_bot/tysontaptapbut"
-              // href=""
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <div className="main3 group">
-                <img
-                  src={gamelogo8}
-                  alt="Description of Image"
-                  loading="lazy"
-                  className="imageclass"
-                />
-                <div className="main4"></div>
-                <span className="spanclass">
-                  <img src={tap}/>
-                </span>
-              </div>
-            </a>
-            <a
-              // href="https://t.me/meme_games_wtf_bot/tysontaptapbut"
-              // href=""
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <div className="main3 group">
-                <img
-                  src={gamelogo9}
-                  alt="Description of Image"
-                  loading="lazy"
-                  className="imageclass"
-                />
-                <div className="main4"></div>
-                <span className="spanclass">
-                  <img src={tap}/>
-                </span>
-              </div>
-            </a>
-            {/* <a href="#" target="_blank" rel="noopener noreferrer"> */}
-            {/* <div className="main3 group">
-              <img
-                src={gamelogo3}
-                alt="Description of Image"
-                className="imageclass"
-              />
-              <div className="main4"></div>
-              <span className="spanclass">COMING SOON</span>
-            </div> */}
-            {/* </a> */}
-            {/*<div className="bg-slate-600 text-white p-4 mt-16 w-52 h-52 flex justify-center items-center rounded-2xl">
-            Coming Soon
-          </div>
-          <div className="bg-slate-600 text-white p-4 mt-16 w-52 h-52 flex justify-center items-center rounded-2xl">
-            Coming Soon
-          </div> */}
+            {gamesData.map((game, index) => (
+              <a
+                key={index}
+                href={game.url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div className="main3 group">
+                  <img
+                    src={game.image}
+                    alt={game.name}
+                    loading="lazy"
+                    className="imageclass"
+                  />
+                  <div className="main4"></div>
+                  <span className="spanclass">
+                    <img src={game.icon} alt="Play Icon" loading="lazy" />
+                  </span>
+                </div>
+              </a>
+            ))}
           </div>
         </div>
       </div>
-      {/* <div className="main5">
-        <p className="main6">Any Meme Game Ideas? Submit Here...</p>
-      </div> */}
       <Footer />
     </>
   );
---
+++ src/components/gamesData.js
@@ -0,0 +1,53 @@
+const gamesData = [
+    {
+      name: "Tap Tap America",
+      url: "http://game.taptapamerica.xyz/",
+      image: "/games/taptapamerica.webp",
+      icon: "/other/play2.png"
+    },
+    {
+      name: "Tyson Tap Tap Butt",
+      url: "https://tysonbutt.memegames.wtf/",
+      image: "/games/tysonbutt2.webp",
+      icon: "/other/play2.png"
+    },
+    {
+      name: "Elon Game",
+      url: "https://elon.memegames.wtf/",
+      image: "/games/dogegame2.webp",
+      icon: "/other/play2.png"
+    },
+    {
+      name: "Doge Game",
+      url: "https://doge.memegames.wtf/",
+      image: "/games/elongame2.webp",
+      icon: "/other/play2.png"
+    },
+    {
+      name: "Whack a Martian",
+      url: "https://whackamartian.memegames.wtf/",
+      image: "/games/martian.webp",
+      icon: "/other/play2.png"
+    },
+    {
+      name: "Let Me Tell You Something",
+      url: "https://letmetellyousomething.memegames.wtf/",
+      image: "/games/something.webp",
+      icon: "/other/play2.png"
+    },
+    {
+      name: "Coming Soon 1",
+      url: null,
+      image: "/games/comingsoon3.webp",
+      icon: "/other/tap.png"
+    },
+    {
+      name: "Coming Soon 2",
+      url: null,
+      image: "/games/comingsoon4.webp",
+      icon: "/other/tap.png"
+    }
+  ];
+  
+  export default gamesData;
+  
