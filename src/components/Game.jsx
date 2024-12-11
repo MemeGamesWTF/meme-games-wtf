7be51d3c799ffcc51c6d335603ade7d062032b3d
MovinduSenura <it22371218@my.sliit.lk>
2024-12-11 12:03:16 +0530

    iframe changed

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -10,13 +10,21 @@ export default function Game() {
     return <div>Game not found</div>;
   }
   return (
-    <>
+    <div style={{backgroundColor: "black"}}>
       {/* <h1>{gameName}</h1> */}
       <iframe
         src={url}
         title={gameName}
-        style={{ width: "100%", height: "100vh", border: "none" }}
+        style={{
+          width: "56.25vh", 
+          height: "100vh", 
+          border: "none", 
+          position: "absolute", 
+          top: "50%", 
+          left: "50%", 
+          transform: "translate(-50%, -50%)"
+        }}
       ></iframe>
-    </>
+    </div>
   );
 }
