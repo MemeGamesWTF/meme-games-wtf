7afe83eb18d9e68d6432b19ade6742fc9b957678
MovinduSenura <movindusenuraaluthge@gmail.com>
2025-03-27 14:07:08 +0530

    phantom wallet

--- src/components/PhantomWallet2.jsx
+++ src/components/PhantomWallet2.jsx
@@ -1,6 +1,14 @@
 import { ConnectButton } from 'thirdweb/react';
+// import { solana } from "thirdweb/chains";
+import { createWallet } from "thirdweb/wallets";
+import { lightTheme } from 'thirdweb/react';
 import { client } from './Client';
 import "./PhantomWallet2.css";
+
+const wallets = [
+  createWallet("app.phantom"), // Add your wallet in wallet list
+  // add other wallets...
+];
 
 function PhantomWallet2() {
   return (
@@ -8,9 +16,24 @@ function PhantomWallet2() {
       {/* <h1>My Web3 App</h1> */}
       <ConnectButton
         client={client}
+        theme={lightTheme({
+          colors: {
+            modalBg: "white",
+            primaryButtonBg: "#9584e2",
+            primaryButtonText: "black",
+            connectedButtonBg: "#9584e2",
+            borderColor: "#9584e2",
+          },
+        })}
+        connectButton={{
+          label: "Connect Phantom Wallet",
+        }}
+        // testnet={true}
+        // chain={solana}
+        wallets={wallets}
         appMetadata={{
           name: 'My Web3 App',
-          url: 'https://memegames.wtf/', // Your app's URL
+          url: 'http://localhost:5173/', // Your app's URL
         }}
       />
     </div>
