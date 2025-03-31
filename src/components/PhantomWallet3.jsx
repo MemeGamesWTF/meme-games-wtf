4901503b33e91bd982f61cd9bfe8ec36d0bcc444
MovinduSenura <movindusenuraaluthge@gmail.com>
2025-03-31 13:18:13 +0530

    test

--- src/components/PhantomWallet3.jsx
+++ src/components/PhantomWallet3.jsx
@@ -106,9 +106,9 @@ export default function PhantomWallet3() {
             <strong>Wallet Connected</strong>
           </p> */}
           {publicKey && (
-            <p className="bg-white px-2 py-1 rounded-[5px] text-center">
+            <p className="bg-white px-2 py-1 rounded-[5px] text-center mb-2">
               {/* <strong>Public Key:</strong>  */}
-              <span className="text-[13px] text-black text-center align-middle justify-center">
+              <span className="text-[13px] text-black">
                 {publicKey.toBase58()}
               </span>
             </p>
