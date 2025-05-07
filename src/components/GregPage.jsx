c0d7e2e16d8d3e3194da400f90f779aa40e61655
MovinduSenura <movindusenuraaluthge@gmail.com>
2025-05-07 10:54:24 +0530

    greg page yt video

--- src/components/GregPage.jsx
+++ src/components/GregPage.jsx
@@ -1,31 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import "./GregPage.css";
+import Footer from "./Footer2";
 import Greg from "/assets/greghead.svg";
 
 export default function GregPage() {
+  const [videoLoaded, setVideoLoaded] = useState(false);
   return (
-    <div className="gp1">
-      <div className="gp2">
-        <img
-          src={Greg}
-          alt="Greg Head"
-          width="180"
-          height="180"
-          className="gpimg"
-        />
-        <h1 className="gph1">Coming Soon</h1>
-        {/* <h1 className="gph2">
+    <>
+      <div className="gp1">
+        <div className="gp2">
+          <img
+            src={Greg}
+            alt="Greg Head"
+            width="180"
+            height="180"
+            className="gpimg"
+          />
+          <h1 className="gph1">Coming Soon</h1>
+          {/* <h1 className="gph2">
         Watch the official trailer now
         </h1> */}
-        <a
+          {/* <a
           href="https://www.youtube.com/watch?v=F8lHr3ku9zQ"
           target="_blank"
           rel="noopener noreferrer"
           className="gplink"
         >
           Click here to watch the official trailer
-        </a>
+        </a> */}
+          <div className="video-container">
+            <iframe
+              width="1120"
+              height="630"
+              src="https://www.youtube.com/embed/F8lHr3ku9zQ"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              onLoad={() => setVideoLoaded(true)}
+              className={`youtube-iframe ${videoLoaded ? "loaded" : ""}`}
+            ></iframe>
+          </div>
+
+          {/* <p className="video-caption">
+          Official Trailer
+        </p> */}
+        </div>
       </div>
-    </div>
+      <div className="gregfooter">
+        <Footer />
+      </div>
+    </>
   );
 }
